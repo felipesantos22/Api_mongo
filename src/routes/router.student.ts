19702355d404b957a03b88cd197ad187d41a1c61
felipesantos22 <felipevs2018@gmail.com>
2023-06-08 23:25:46 -0300

    finalmente conexão com mongodb foi realizada

--- src/routes/router.student.ts
+++ src/routes/router.student.ts
@@ -1,14 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import createStudentController from '../controller/student';
 
 const routerStudent = express.Router();
-
-mongoose.connect('mongodb://localhost/schooltest');
-
-routerStudent.get('/student', (req, res) => {
-    console.log("Mongoose")
-});
 
 routerStudent.post('/student', createStudentController);
 
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import routerStudent from '../routes/router.student';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
 
+mongoose.connect('mongodb://root:password@localhost/schooltest?authSource=admin').then(() => {
+    console.log("Conected.")
+}).catch(() => {
+    console.log("Failed.")
+});
+
 app.use(routerStudent);
 
 export default app;
+
